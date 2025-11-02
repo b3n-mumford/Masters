e34a9670d5b834a7ce9e261bdc1223db887b9a4d
Ben Mumford <benmumford27@gmail.com>
2025-11-02 14:19:13 -0500

    commit

---
+++ scripts/Data_setup.R
@@ -0,0 +1,8 @@
+# library(here)
+# 
+# # Example: read raw data
+# data <- read.csv(here("..", "Data", "raw", "stations.csv"))
+# 
+# # Save processed data
+# write.csv(data, here("..", "Data", "processed", "stations_clean.csv"))
+z`z`
